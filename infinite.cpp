e631db28e9147d3b0580b1ba9b2d21584ce4d1a3
ibs-harry-no1 <102588404+ibs-harry-no1@users.noreply.github.com>
2022-03-29 02:39:13 -0700

    Create infinite.cpp

---
+++ infinite.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Inida is my country and all indians are my brother and ...\n";
+    int *a = new int[4];
+    while(true){
+        cout<<"Shirz is ...\n";
+    }
+}
